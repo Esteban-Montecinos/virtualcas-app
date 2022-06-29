f1366e82b6b515599e48faec8cf240b6ca228c30
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2022-06-29 18:31:46 -0400

    paginas de menu

---
+++ src/components/pages/menu/VerMovimientos.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const VerMovimientos = () => {
+  return (
+    <Text>VerMovimientos</Text>
+  )
+}
+
+export default VerMovimientos
